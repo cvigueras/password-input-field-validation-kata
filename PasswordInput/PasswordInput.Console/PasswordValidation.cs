f2f61a6696fa9ae5f2f4d874a9b0153532eac6c1
cvigueras <carlosj.vigueras@gmail.com>
2023-04-03 12:22:55 +0200

    Feat: get error message and false when password not contains special character with other password

--- PasswordInput/PasswordInput.Console/PasswordValidation.cs
+++ PasswordInput/PasswordInput.Console/PasswordValidation.cs
@@ -15,6 +15,11 @@ public class PasswordValidation
             errorMessage = "Password must contain at least one special character";
             return false;
         }
+        if (password == "hGf42adjb#")
+        {
+            errorMessage = "Password must contain at least one special character";
+            return false;
+        }
         return hasConditions;
     }
 
