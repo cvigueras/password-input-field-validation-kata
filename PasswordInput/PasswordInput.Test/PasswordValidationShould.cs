d39b57bc94c21714ef66958dd2dab917a52b14aa
cvigueras <carlosj.vigueras@gmail.com>
2023-04-03 11:03:17 +0200

    Test: get_error_message_and_false_when_password_is_null

--- PasswordInput/PasswordInput.Test/PasswordValidationShould.cs
+++ PasswordInput/PasswordInput.Test/PasswordValidationShould.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+
 namespace PasswordInput.Test
 {
     public class PasswordValidationShould
@@ -5,6 +7,25 @@ namespace PasswordInput.Test
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public void get_error_message_and_false_when_password_is_null()
+        {
+            var passwordValidation = new PasswordValidation();
+
+            var result = passwordValidation.CheckConditions(null, out var errorMessage);
+
+            errorMessage.Should().Be("Password must be at least 8 characters");
+            result.Should().Be(false);
+        }
+    }
+
+    public class PasswordValidation
+    {
+        public object CheckConditions(object o, out object o1)
+        {
+            throw new NotImplementedException();
         }
     }
 }
