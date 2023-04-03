20b5e82813b00384434b8a9efa0dff2363df0dae
cvigueras <carlosj.vigueras@gmail.com>
2023-04-03 13:22:51 +0200

    Refctor: moved methods to password class

--- PasswordInput/PasswordInput.Console/Password.cs
+++ PasswordInput/PasswordInput.Console/Password.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+
 namespace PasswordInput.Console;
 
 public class Password
@@ -29,6 +31,37 @@ public class Password
         ErrorMessage += errorMessage;
     }
 
+
+    public bool CheckHasEspecialCharacter()
+    {
+        var regex = new Regex("[^a-zA-Z0-9]{1,}");
+        if (!string.IsNullOrEmpty(Value) && regex.IsMatch(Value)) return true;
+        SetErrorMessage("Password must contain at least one special character");
+        return false;
+    }
+
+    public bool CheckHasCapitalLetter()
+    {
+        var regex = new Regex("[A-Z]{1,}");
+        if (!string.IsNullOrEmpty(Value) && regex.IsMatch(Value)) return true;
+        SetErrorMessage("Password must contain at least one capital letter");
+        return false;
+    }
+
+    public bool CheckHasLength()
+    {
+        if (!string.IsNullOrEmpty(Value) && Value.Length >= 8) return true;
+        SetErrorMessage("Password must be at least 8 characters");
+        return false;
+    }
+
+    public bool CheckHasAtLeastTwoNumbers()
+    {
+        var regex = new Regex(@"(\D*\d){2,}");
+        if (!string.IsNullOrEmpty(Value) && regex.IsMatch(Value)) return true;
+        SetErrorMessage("The password must contain at least 2 numbers");
+        return false;
+    }
     private void SetPasswordIsValid()
     {
         PasswordIsValid = false;
--- PasswordInput/PasswordInput.Console/PasswordValidation.cs
+++ PasswordInput/PasswordInput.Console/PasswordValidation.cs
@@ -6,54 +6,10 @@ public class PasswordValidation
 {
     public bool CheckConditions(Password password)
     {
-        bool hasConditions = CheckHasLength(password);
-        hasConditions = CheckHasAtLeastTwoNumbers(password);
-        hasConditions = CheckHasCapitalLetter(password);
-        hasConditions = CheckHasEspecialCharacter(password);
+        var hasConditions = password.CheckHasLength();
+        hasConditions = password.CheckHasAtLeastTwoNumbers();
+        hasConditions = password.CheckHasCapitalLetter();
+        hasConditions = password.CheckHasEspecialCharacter();
         return hasConditions;
-    }
-
-    private static bool CheckHasEspecialCharacter(Password password)
-    {
-        var regex = new Regex("[^a-zA-Z0-9]{1,}");
-        if (string.IsNullOrEmpty(password.Value) || !regex.IsMatch(password.Value))
-        {
-            password.SetErrorMessage("Password must contain at least one special character");
-            return false;
-        }
-        return true;
-    }
-
-    private static bool CheckHasCapitalLetter(Password password)
-    {
-        var regex = new Regex("[A-Z]{1,}");
-        if (string.IsNullOrEmpty(password.Value) || !regex.IsMatch(password.Value))
-        {
-            password.SetErrorMessage("Password must contain at least one capital letter");
-            return false;
-        }
-        return true;
-    }
-
-    private static bool CheckHasLength(Password password)
-    {
-        if (string.IsNullOrEmpty(password.Value) || password.Value.Length < 8)
-        {
-            password.SetErrorMessage("Password must be at least 8 characters");
-            return false;
-        }
-        return true;
-    }
-
-    private static bool CheckHasAtLeastTwoNumbers(Password password)
-    {
-        var regex = new Regex(@"(\D*\d){2,}");
-        if (string.IsNullOrEmpty(password.Value) || !regex.IsMatch(password.Value))
-        {
-            password.SetErrorMessage("The password must contain at least 2 numbers");
-            return false;
-
-        }
-        return true;
     }
 }
